3236568b6ad6f2b4ea61718d3661a75c0e5c8735
Jeremy Behreandt <behreandtjeremy@gmail.com>
2023-09-16 23:29:11 -0500

    Match editorconfig to Godot editor settings.

--- Bridge.gd
+++ Bridge.gd
@@ -4,10 +4,10 @@ const Rgb = preload("res://Rgb.gd")
 ## native objects.
 class Bridge:
 
-	## Converts an RGB color to a GDScript Color.
-	static func gamma_rgb_to_color(c: Rgb.Rgb) -> Color:
-		return Color(c.r, c.g, c.b, c.alpha)
-		
-	## Converts a GDScript Color to an RGB color.
-	static func color_to_gamma_rgb(c: Color) -> Rgb.Rgb:
-		return Rgb.Rgb.new(c.r, c.g, c.b, c.a)
+    ## Converts an RGB color to a GDScript Color.
+    static func gamma_rgb_to_color(c: Rgb.Rgb) -> Color:
+        return Color(c.r, c.g, c.b, c.alpha)
+
+    ## Converts a GDScript Color to an RGB color.
+    static func color_to_gamma_rgb(c: Color) -> Rgb.Rgb:
+        return Rgb.Rgb.new(c.r, c.g, c.b, c.a)
